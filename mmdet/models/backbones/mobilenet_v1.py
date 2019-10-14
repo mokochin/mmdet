7a31747bbef988289cc20274c7ff49d174ad1bbf
mokochin <657171818@qq.com>
2019-10-14 10:44:25 +0800

    test

--- mmdet/models/backbones/mobilenet_v1.py
+++ mmdet/models/backbones/mobilenet_v1.py
@@ -250,3 +250,6 @@ class MbNet_V1(nn.Module):
                 # trick: eval have effect on BatchNorm only
                 if isinstance(m, _BatchNorm):
                     m.eval()
+net=MbNet_V1(28)
+net.eval()
+print(net)

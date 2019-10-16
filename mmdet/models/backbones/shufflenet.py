76a1e4ddbd4ae800abea62f7c2d93678c4efcc10
mokochin <657171818@qq.com>
2019-10-16 18:57:35 +0800

    10161857

--- mmdet/models/backbones/shufflenet.py
+++ mmdet/models/backbones/shufflenet.py
@@ -6,7 +6,7 @@ import torch.utils.checkpoint as cp
 from mmcv.cnn import constant_init, kaiming_init
 from mmcv.runner import load_checkpoint
 from torch.nn.modules.batchnorm import _BatchNorm
-
+from collections import OrderedDict
 from mmdet.models.plugins import GeneralizedAttention
 from mmdet.ops import ContextBlock, DeformConv, ModulatedDeformConv
 from ..registry import BACKBONES
@@ -32,7 +32,6 @@ def channel_shuffle(x, groups):
     return x
 
 class ShuffleUnit(nn.Module):
-    expansion = 4
 
     def __init__(self,
                  in_channels,
@@ -62,13 +61,13 @@ class ShuffleUnit(nn.Module):
         self.norm3_name, norm3 = build_norm_layer(norm_cfg, self.out_channels, postfix=3)  #norm3由conv3 expand使用
 
         #定义bottleneck的尺寸
-        self.bottleneck_channels = in_channels//4
-        self.first_1x1_groups = self.groups if grouped_conv else 1
+        self.bottleneck_channels = in_channels//4 #每个stage_0都不是这样,需要修改
+        self.first_1x1_groups = self.groups if grouped_conv else 1 #stage2_0的第一层
 
         #compress:compress1x1、bn、relu
         self.conv1 = build_conv_layer(
             conv_cfg, in_channels, self.bottleneck_channels,
-            kernel_size=1, stride=stride, padding=padding, bias=bias, groups=self.groups)
+            kernel_size=1, stride=stride, padding=padding, bias=bias, groups=self.first_1x1_groups)
         self.add_module(self.norm1_name, norm1) #nn里面添加层的方法 net1.add_module('batchnorm', nn.BatchNorm2d(3))
 
         #bottleneck:dw_conv、bn
@@ -79,7 +78,7 @@ class ShuffleUnit(nn.Module):
 
         #expand:expand1x1、bn
         self.conv3 = build_conv_layer(
-            conv_cfg, self.bottleneck_channels, out_channels,
+            conv_cfg, self.bottleneck_channels, self.out_channels,
             kernel_size=1, group=self.groups)
         self.add_module(self.norm3_name, norm3)
 
@@ -113,6 +112,24 @@ class ShuffleUnit(nn.Module):
         return torch.cat((x, out), 1)
         # define the type of ShuffleUnit
 
+    def _make_grouped_conv1x1(self, in_channels, out_channels, groups,
+                              batch_norm=True, relu=False):
+
+        modules = OrderedDict()
+
+        conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, groups=groups)
+
+        modules['conv1x1'] = conv
+
+        if batch_norm:
+            modules['batch_norm'] = nn.BatchNorm2d(out_channels)
+        if relu:
+            modules['relu'] = nn.ReLU()
+        if len(modules) > 1:
+            return nn.Sequential(modules)
+        else:
+            return conv
+
     @property
     def norm1(self):
         return getattr(self, self.norm1_name) # getattr返回一个对象的属性值
@@ -131,6 +148,8 @@ class ShuffleUnit(nn.Module):
         out = self.conv1(x)
         out = self.norm1(out)
         out = self.relu(out)
+
+        out = channel_shuffle(out, self.groups)
 
         out = self.conv2(out)
         out = self.norm2(out)
@@ -183,34 +202,18 @@ def make_shuffle_layer(block, in_channels, out_channels, blocks,
 class ShuffleNet(nn.Module):
 
     arch_settings = {
-        1: (ShuffleUnit, (144, 288, 576)),
-        2: (ShuffleUnit, (200, 400, 800)),
-        3: (ShuffleUnit, (240, 480, 960)),
-        4: (ShuffleUnit, (272, 544, 1088)),
-        8: (ShuffleUnit, (384, 768, 1536))
-    }
-
-    blocks_settings = (3, 7, 3)
-
-    #可能没用
-    stride_settings = {
-        0: (2, 1, 1, 1),
-        1: (2, 1, 1, 1, 1, 1, 1, 1),
-        2: (2, 1, 1, 1),
-    }
-
-    plane_settings = {
-        0: (2, 1, 1, 1),
-        1: (2, 1, 1, 1, 1, 1, 1, 1),
-        2: (2, 1, 1, 1),
+        1: (ShuffleUnit, (144, 288, 576), (4, 8, 4)),#1,2,4,8待修改
+        2: (ShuffleUnit, (200, 400, 800), (4, 8, 4)),
+        3: (ShuffleUnit, (60, 120, 240), (240, 480, 960), (4, 8, 4)),
+        4: (ShuffleUnit, (272, 544, 1088), (4, 8, 4)),
+        8: (ShuffleUnit, (384, 768, 1536), (4, 8, 4))
     }
 
     def __init__(self,
-                 depth,
-                 num_stages=4,
-                 strides=(1, 2, 2, 2),
-                 dilations=(1, 1, 1, 1),
-                 out_indices=(0, 1, 2, 3),
+                 g_groups,
+                 num_stages=3,
+                 in_channels=24,
+                 out_indices=(0, 1, 2),
                  style='pytorch',
                  frozen_stages=-1,
                  conv_cfg=None,
@@ -219,14 +222,11 @@ class ShuffleNet(nn.Module):
                  with_cp=False,
                  zero_init_residual=True):
         super(ShuffleNet, self).__init__()
-        if depth not in self.arch_settings:
-            raise KeyError('invalid depth {} for resnet'.format(depth))
-        self.depth = depth
+        if g_groups not in self.arch_settings:
+            raise KeyError('invalid g_groups {} for SNet'.format(g_groups))
+        self.g_groups = g_groups
         self.num_stages = num_stages
-        assert num_stages >= 1 and num_stages <= 4
-        self.strides = strides
-        self.dilations = dilations
-        assert len(strides) == len(dilations) == num_stages
+        assert num_stages >= 1 and num_stages <= 3
         self.out_indices = out_indices
         assert max(out_indices) < num_stages
         self.style = style
@@ -236,38 +236,34 @@ class ShuffleNet(nn.Module):
         self.with_cp = with_cp
         self.norm_eval = norm_eval
         self.zero_init_residual = zero_init_residual
-        self.block, stage_blocks = self.arch_settings[depth]
+        self.block, bottleneck_channels, out_channels, stage_blocks = self.arch_settings[self.g_groups]
         self.stage_blocks = stage_blocks[:num_stages]
-        self.inplanes = 64
+        self.bottleneck_channels = bottleneck_channels[:num_stages]
+        self.out_channels=out_channels[:num_stages]
+        self.in_channels=in_channels
 
         self._make_stem_layer()
 
-        self.res_layers = []
+        self.sn_layers = []
         for i, num_blocks in enumerate(self.stage_blocks):
-            stride = strides[i]
-            dilation = dilations[i]
-            planes = 64 * 2**i
-            res_layer = make_shuffle_layer(
+            sn_layer = make_shuffle_layer(
                 self.block,
-                self.inplanes,
-                planes,
+                self.in_channels,
+                self.bottleneck_channels[i],
+                self.out_channels[i],
                 num_blocks,
-                stride=stride,
-                dilation=dilation,
                 style=self.style,
                 with_cp=with_cp,
                 conv_cfg=conv_cfg,
                 norm_cfg=norm_cfg,
                 )
-            self.inplanes = planes * self.block.expansion
             layer_name = 'layer{}'.format(i + 1) #更改layer name 好用
-            self.add_module(layer_name, res_layer)
-            self.res_layers.append(layer_name)
+            self.add_module(layer_name, sn_layer)
+            self.sn_layers.append(layer_name)
+            self.in_channels=self.out_channels[i]
 
         self._freeze_stages()
 
-        self.feat_dim = self.block.expansion * 64 * 2**(
-            len(self.stage_blocks) - 1)
 
     def _make_stem_layer(self): #这个是最前面的层
         self.conv1 = build_conv_layer(
@@ -276,6 +272,7 @@ class ShuffleNet(nn.Module):
             24,
             kernel_size=3,
             stride=2,
+            padding=1,
             bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 

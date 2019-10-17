f4f91fff45fda7ff68dbeb85b225198624c9f524
mokochin <657171818@qq.com>
2019-10-17 12:08:48 +0800

    11

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -3,6 +3,6 @@ from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .mobilenet_v1 import MbNet_V1
-from .shufflenet import ShuffleNet
+# from .shufflenet import ShuffleNet
 
-__all__ = ['ResNet', 'MbNet_V1', 'ShuffleNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'MbNet_V1', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
